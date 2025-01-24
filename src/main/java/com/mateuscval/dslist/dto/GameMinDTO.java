42dc0368ea849d36d9d11b44df58cf58c3f275bb
Mateus Amaral <mateuscval@gmail.com>
2025-01-24 09:15:08 -0300

    Deploy ready

--- src/main/java/com/mateuscval/dslist/dto/GameMinDTO.java
+++ src/main/java/com/mateuscval/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.mateuscval.dslist.dto;
 
 import com.mateuscval.dslist.entities.Game;
+import com.mateuscval.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	private Long id;
@@ -21,6 +22,14 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+	
 	public Long getId() {
 		return id;
 	}
